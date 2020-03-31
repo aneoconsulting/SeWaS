d5a8c73d822f5f032d620a12265c71dc11ba78da
Salli Moustafa <moustafasalli@gmail.com>
2020-03-31 13:44:44 +0200

    Fix link error on Windows

--- include/LogManager.hxx
+++ include/LogManager.hxx
@@ -24,7 +24,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/async.h>
 #include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/ansicolor_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 #include "OS.hxx"
 #include "Constants.hxx"
