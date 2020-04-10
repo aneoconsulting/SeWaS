2d39ed486d43f8190db06870ecb3be8bdf4b5d23
Salli Moustafa <moustafasalli@gmail.com>
2020-04-10 16:21:31 +0200

    Make sure the found vcpkg is usable

--- scripts/bootstrap-sewas.sh
+++ scripts/bootstrap-sewas.sh
@@ -8,7 +8,8 @@ VCPKG_BUILD=$SEWAS_ROOT/thirdparty/vcpkg/build
 echo "[START] Bootstraping"
 
 _vcpkg=`PATH=$VCPKG_ROOT:$PATH command -v vcpkg`
-if [ "$_vcpkg" = "" ]; then
+_vcpkg_toolchain_file=$(realpath `dirname $_vcpkg`)/scripts/buildsystems/vcpkg.cmake
+if [ "$_vcpkg" = "" || ! -f $_vcpkg_toolchain_file ]; then
 	echo "vcpkg has not been found"
 	
 	echo "[START] install vcpkg"
