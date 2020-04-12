d1a4508d5e10b9084fd4468abb8757e5ea26b41b
Salli Moustafa <moustafasalli@gmail.com>
2020-04-12 11:27:03 +0200

    Rework Github Actions configuration
    Add Windows build job and some fix bugs that
    prevent correct detection/installation of vcpkg

--- scripts/bootstrap-sewas.ps1
+++ scripts/bootstrap-sewas.ps1
@@ -5,7 +5,8 @@ $VCPKG_BUILD="$SEWAS_ROOT/thirdparty/vcpkg/build"
 
 Write-Output "[START] Bootstraping"
 
-$_vcpkg_not_found=!@("(Get-Command vcpkg.exe)" -or (Test-Path "$VCPKG_ROOT/vcpkg.exe"))
+$_vcpkg="$VCPKG_ROOT/vcpkg.exe"
+$_vcpkg_not_found=!@(Test-Path $_vcpkg)
 if ($_vcpkg_not_found)
 {
     Write-Output "vcpkg has not been found"
@@ -17,25 +18,27 @@ if ($_vcpkg_not_found)
         if (Test-Path "$VCPKG_BUILD")
         {
             Remove-Item -Recurse "$VCPKG_BUILD" -ErrorAction Stop
-            New-Item -Path "$VCPKG_BUILD" -ItemType "directory" -ErrorAction Stop
-		}
-
+	    }
+        New-Item -Path "$VCPKG_BUILD" -ItemType "directory" -ErrorAction Stop
+	
         Copy-Item "$SEWAS_ROOT/cmake/resources/vcpkg/CMakeLists.txt" -Destination "$VCPKG_BUILD" -ErrorAction Stop
-
-        (cd "$VCPKG_BUILD") -and (cmake .) -and (cmake --build .)
-	}
+	
+        Set-Location "$VCPKG_BUILD"
+    	cmake .
+	    cmake --build .
+    }
     catch
     {
-        Write-Error "Failed to install vcpkg : $Error"
-	}
+        throw "Failed to install vcpkg : $Error"
+    }
 
     Write-Output "[STOP] install vcpkg"
 }
 else
-{
-    If (Test-Path "$VCPKG_ROOT/vcpkg.exe") {$_vcpkg="$VCPKG_ROOT/vcpkg.exe"} Else {$_vcpkg=(Get-Command vcpkg.exe).Source}
-    
+{    
     Write-Output "vcpkg found : $_vcpkg"
+    & $_vcpkg version
+    & $_vcpkg update    
 }
 
 Write-Output "[STOP] Bootstraping"
--- scripts/bootstrap-sewas.sh
+++ scripts/bootstrap-sewas.sh
@@ -7,8 +7,8 @@ VCPKG_BUILD=$SEWAS_ROOT/thirdparty/vcpkg/build
 
 echo "[START] Bootstraping"
 
-_vcpkg=`PATH=$VCPKG_ROOT:$PATH command -v vcpkg`
-_vcpkg_toolchain_file=$(realpath `dirname $_vcpkg`)/scripts/buildsystems/vcpkg.cmake
+_vcpkg=`command -v $VCPKG_ROOT/vcpkg`
+_vcpkg_toolchain_file=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
 if [ "$_vcpkg" = "" || ! -f $_vcpkg_toolchain_file ]; then
 	echo "vcpkg has not been found"
 	
@@ -23,6 +23,8 @@ if [ "$_vcpkg" = "" || ! -f $_vcpkg_toolchain_file ]; then
 	echo "[STOP] install vcpkg"
 else
 	echo "vcpkg found : " $_vcpkg
+	$vcpkg version
+	$vcpkg update
 fi
 
 echo "[STOP] Bootstraping"
