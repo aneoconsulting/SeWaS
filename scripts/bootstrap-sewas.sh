dfd456fdf1eefca81a59e16d9dfb4e9491b3dd63
Salli Moustafa <moustafasalli@gmail.com>
2020-04-12 12:44:15 +0200

    First check for existance of vcpkg root

--- scripts/bootstrap-sewas.sh
+++ scripts/bootstrap-sewas.sh
@@ -7,9 +7,8 @@ VCPKG_BUILD=$SEWAS_ROOT/thirdparty/vcpkg/build
 
 echo "[START] Bootstraping"
 
-ls $VCPKG_ROOT
 _vcpkg=`command -v $VCPKG_ROOT/vcpkg`
-if [[ -x "$VCPKG_ROOT/vcpkg" ]]; then
+if [[ ! -d "$VCPKG_ROOT" || ! -x "$VCPKG_ROOT/vcpkg" ]]; then
 	echo "vcpkg has not been found"
 	
 	echo "[START] install vcpkg"
